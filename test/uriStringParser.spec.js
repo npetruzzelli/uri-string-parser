69abc9c5c518fcf412a709c00b09eb871ce65af5
Nick Petruzzelli <code.npetruzzelli@gmail.com>
2018-05-08 14:38:19 -0400

    Fix ReferenceError in `uriStringParser.spec.js`
    Signed-off-by: Nick Petruzzelli <code.npetruzzelli@gmail.com>

--- test/uriStringParser.spec.js
+++ test/uriStringParser.spec.js
@@ -2,8 +2,8 @@
 const chai = require('chai')
 const expect = chai.expect
 const uriStringParser = require('../lib/index')
-const parseURI = ursParser.uri
-const parseURL = ursParser.url
+const parseURI = uriStringParser.uri
+const parseURL = uriStringParser.url
 
 describe('parseUniformResource', function() {
   it('should have tests written for it!')
